eee32fc15d6b6f6abe0cb66deafc08909126ff6d
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-17 20:40:49 -0400

    added pool.js to modules and changed database to wknd-4-gallery

---
+++ server/modules/pool.js
@@ -0,0 +1,34 @@
+//import PG
+const pg = require('pg');
+const url = require('url');
+
+//POOL config
+let config = {};
+
+if (process.env.DATABASE_URL) {
+  config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false},
+  };
+} else {
+  config = {
+    database: 'wknd-4-gallery', 
+    host: 'localhost', 
+    port: 5432, 
+    max: 10, 
+    idleTimeoutMillis: 30000 
+  };
+}
+
+const pool = new pg.Pool(config);
+
+pool.on("connect", () => {
+  console.log("connected to postgres");
+});
+
+pool.on("error", (err) => {
+  console.log("error connecting to postgres", err);
+});
+
+module.exports = pool;
+
