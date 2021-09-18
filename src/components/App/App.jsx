e8776cc6fd3d2cde8f5f74fe43743a9d55ff0841
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-17 22:50:24 -0400

    updated app.js - created const showFunction, created function handleImgDescToggle to toggle showFunction, and added ternary operator to post-content div that uses showImage to determine img or description

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [postList, setPostList] = useState([]);
   const [newPostDescription, setNewPostDescription] = useState('');
   const [newPostPath, setNewPostPath] = useState('');
+  const [showImage, setShowImage] = useState(true);
 
   //Page Ready - Calls GET function on page load
   useEffect(() => {
@@ -27,31 +28,36 @@ function App() {
     })
   }
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <div className="Subheader">
-          <h3 className="subheader-title">Picture Gallery</h3>
-        </div>
-        <div className="gallery-container">
-          {postList.map(post => (
-              <div className="post-container" key={post.id}>
-                <div className="post-content">
-                  <img src={post.path} />
-                </div>
-              </div>
-          ))}
-        </div>
-        <div className="Subheader">
-          <h3 className="subheader-title">Add a New Post</h3>
-        </div>
-        <div className="galleryform-container">
-          <h4>form with inputs and add post button here</h4>
-        </div>
+  const handleImgDescToggle = () => {
+    console.log('Post clicked - toggle');
+    setShowImage(!showImage);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <div className="Subheader">
+        <h3 className="subheader-title">Picture Gallery</h3>
       </div>
-    );
+      <div className="gallery-container">
+        {postList.map(post => (
+            <div className="post-container" key={post.id}>
+              <div className="post-content" onClick={() => handleImgDescToggle()}>
+                {showImage ? (<img src={post.path} />) : (<p>{post.description}</p>)}
+              </div>
+            </div>
+        ))}
+      </div>
+      <div className="Subheader">
+        <h3 className="subheader-title">Add a New Post</h3>
+      </div>
+      <div className="galleryform-container">
+        <h4>form with inputs and add post button here</h4>
+      </div>
+    </div>
+  );
 }
 
 export default App;
