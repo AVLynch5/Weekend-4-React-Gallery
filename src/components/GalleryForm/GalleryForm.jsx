2625de37e41ce0b61c823b29180e53e85a723fd7
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-19 15:58:38 -0400

    galleryform - edited button text and changed bitton plus icon size to small

--- src/components/GalleryForm/GalleryForm.jsx
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -19,7 +19,7 @@ function GalleryForm({addPost, setNewPostDescription, setNewPostPath, newPostDes
             <form onSubmit={handleAddPost}>
                 <input value={newPostDescription} id="description" placeholder="Image Description" onChange={(event) => setNewPostDescription(event.target.value)}/>
                 <input value={newPostPath} id="path" placeholder="Image Path" onChange={(event) => setNewPostPath(event.target.value)}/>
-                <Button variant="contained" endIcon={<AddCircleIcon />} type="submit">Add New Post</Button>
+                <Button size="small" variant="contained" endIcon={<AddCircleIcon fontSize="small"/>} type="submit">Add Post</Button>
             </form>
         </div>
         </>
