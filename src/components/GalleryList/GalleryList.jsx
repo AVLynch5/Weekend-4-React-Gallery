05172ee82ba7f847c14d180bb54a9edc2ab826fa
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-19 16:44:45 -0400

    inside div gallery-container, wrapped list.map within a Grid npm run client

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,5 +1,6 @@
 import GalleryItem from "../GalleryItem/GalleryItem";
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 function GalleryList({list, deletePost, putLikePost}) {
     return(
@@ -9,6 +10,13 @@ function GalleryList({list, deletePost, putLikePost}) {
             </div>
             <Container>
                 <div className="gallery-container">
+                <Grid
+                    container
+                    direction="row-reverse"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={4}
+                >
                     {list.map(post => 
                         (<GalleryItem
                             key={post.id}
@@ -17,6 +25,7 @@ function GalleryList({list, deletePost, putLikePost}) {
                             deletePost={deletePost}
                         />)
                     )}
+                </Grid>
                 </div>
             </Container>
         </>
