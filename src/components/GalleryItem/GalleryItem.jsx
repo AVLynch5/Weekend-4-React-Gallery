0e3cc0d0007fe7fcfffabe8696758412a91f650c
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-19 21:42:44 -0400

    updated GalleryItem with code comments

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -7,20 +7,25 @@ import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import Box from '@mui/material/Box';
 
 function GalleryItem({post, putLikePost, deletePost}) {
+    //destructuring
     const [showImage, setShowImage] = useState(true);
+    //function toggles value of showImage from true->false and vice versa
     const handleImgDescToggle = () => {
         console.log('Post clicked - toggle');
         setShowImage(!showImage);
     }
+    //function called on like button click. Takes postId and postLikes as params and passes them onto putLikePost
     const handlePostLike = (postId, postLikes) => {
         console.log('User liked post with postId', postId);
         putLikePost(postId, postLikes);
     }
+    //function called on delete button click. Takes PostId as param and passes it onto deletePost.
     const handlePostDelete = (postId) => {
         console.log('User deleted post with ID', postId);
         deletePost(postId);
     }
     
+    //Rendered on the DOM - ternary operator handles image vs description conditional rendering
     return(
         <Box className="post-container" border={3} borderColor="black" pt={2} width={250} height={300}>
             <div className="post-content" onClick={() => handleImgDescToggle()}>
