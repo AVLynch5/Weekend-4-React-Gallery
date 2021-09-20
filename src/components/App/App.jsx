796cd064bc6654dd5b39ce1f0e8747e0a4eabdda
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-19 21:22:47 -0400

    updated app.jsx with code comments

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,12 @@
+//import list
 import React from 'react';
 import './App.css';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
+//components
 import GalleryForm from '../GalleryForm/GalleryForm';
 import GalleryList from '../GalleryList/GalleryList';
+//MUI
 import Container from '@mui/material/Container';
 
 function App() {
@@ -18,6 +21,8 @@ function App() {
     getGallery();
   }, []);
 
+
+  //GET function - updates value of postList array with DB objects
   const getGallery = () => {
     axios({
       method: 'GET',
@@ -30,6 +35,7 @@ function App() {
     })
   }
 
+  //PUT function - called by handle function - takes postId and postLikes as arguments. Sends postId as route param and postLikes as data object, then calls GET function.
   const putLikePost = (postId, postLikes) => {
     axios({
       method: 'PUT',
@@ -42,6 +48,7 @@ function App() {
     })
   }
 
+  //DELETE function - takes postId as input param and sends as route param, then calls GET function
   const deletePost = (postId) => {
     axios({
       method: 'DELETE',
@@ -53,6 +60,7 @@ function App() {
     })
   }
 
+  //POST function - event triggered by submit button click on the DOM. Prevent default stops default form behavior. Sends object containing form inputs to the DB.
   const addPost = (event) => {
     event.preventDefault();
     console.log('Submitted form to add new post');
@@ -67,17 +75,22 @@ function App() {
     })
   }
 
+  //Render on the DOM
   return (
     <div className="App">
+      {/* Add header code to a component and import it here */}
       <header className="App-header">
         <h1 className="App-title">Gallery of My Life</h1>
       </header>
+      {/* MUI container SHOULD center contents horizontally - flexbox. sx param - automatic margins */}
       <Container sx={{mx: 'auto'}}>
+        {/* Calls GalleryList component containing .map and elements to be rendered. Calls required props */}
         <GalleryList
           list={postList}
           deletePost={deletePost}
           putLikePost={putLikePost}
         />
+        {/* Calls GalleryForm component containing new post inputs and submit button. Calls required props */}
         <GalleryForm
           addPost={addPost}
           setNewPostDescription={setNewPostDescription}
