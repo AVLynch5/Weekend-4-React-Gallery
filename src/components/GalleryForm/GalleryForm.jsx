0f25d13dcba2f8d45cfd08b23d33ef68934f864c
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-21 15:26:53 -0400

    imported GalleryForm.css into GalleryForm.jsx. Applied styling to GalleryForm and GalleryList. Cannot adjust width of GalleryForm without losing centering

--- src/components/GalleryForm/GalleryForm.jsx
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -1,7 +1,9 @@
+import './GalleryForm.css';
 //MUI
 import Button from '@mui/material/Button';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 //never got to this one... sweetAlerts make warning/delete alerts easier
 import Alert from '@mui/material/Alert';
 
@@ -19,14 +21,14 @@ function GalleryForm({addPost, setNewPostDescription, setNewPostPath, newPostDes
         <div className="Subheader">
             <h3 className="subheader-title">Add a New Post</h3>
         </div>
-        <div className="galleryform-container">
+        <Box className="galleryform-container" border={3} borderColor="black" pt={2}>
             {/* On button click, handleAddPost called. Info entered into text fields sent to DB. Textfield required props mean inputs must be filled out.  */}
             <form onSubmit={handleAddPost}>
                 <TextField required size="small" value={newPostDescription} id="description" label="Image Description" type="text" onChange={(event) => setNewPostDescription(event.target.value)}/>
                 <TextField required size="small" value={newPostPath} id="path" label="Image Path" type="url" onChange={(event) => setNewPostPath(event.target.value)}/>
                 <Button style={{height: 39}} variant="contained" endIcon={<AddCircleIcon fontSize="small"/>} type="submit">Add Post</Button>
             </form>
-        </div>
+        </Box>
         </>
     );
 }
